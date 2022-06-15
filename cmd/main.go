510c19c338e5894c582c157b88516043f3ea2857
Charlie Jonas <charlie@charliejonas.co.uk>
2022-06-15 23:14:05 +0100

    Add flag to print version

--- cmd/main.go
+++ cmd/main.go
@@ -16,6 +16,9 @@ var version = "dev-edge"
 // The dnstap UNIX socket path and HTTP webserver port.
 var sockPath, webPort string
 
+// If the --version or -v command line option has been invoked.
+var printVerAndExit bool
+
 var rootCmd = &cobra.Command{
 	Use:   "whoami-dns",
 	Short: "HTTP server that works in tandem with an authoritative DNS server using dnstap",
@@ -23,6 +26,11 @@ var rootCmd = &cobra.Command{
 		"server using dnstap and wildcard domains so that clients' recursive DNS servers can " +
 		"be identified by the source IP addresses of their queries.",
 	Run: func(cmd *cobra.Command, args []string) {
+		if printVerAndExit {
+			fmt.Println("whoami-dns version", version)
+			os.Exit(0)
+		}
+
 		serv := whoami.NewServer()
 		serv.SetHeader("X-Powered-By", "https://github.com/CHTJonas/whoami-dns")
 		serv.SetHeader("X-Version", version)
@@ -44,6 +52,7 @@ func init() {
 	cobra.OnInitialize(initConfig)
 	rootCmd.Flags().StringVarP(&sockPath, "bind", "b", "/var/lib/knot/dnstap.sock", "path to dnstap UNIX socket")
 	rootCmd.Flags().StringVarP(&webPort, "port", "p", "6780", "port on which to listen for HTTP requests")
+	rootCmd.Flags().BoolVarP(&printVerAndExit, "version", "v", false, "print version and exit")
 }
 
 func initConfig() {
